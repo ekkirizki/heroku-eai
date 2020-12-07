3e7c2548f682dbf5a6f83a1518699d6351856a1a
Ekki Rizki Ramadhan <ekkirizki@gmail.com>
2020-12-07 10:47:29 +0700

    setting db

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$Database_url = parse_url(getenv("DATABASE_URL"));
+$Database_url = parse_url('postgres://aphxbojuupetdq:b754335b62446d2ed456cf8b69170797be673a53c6a9833c2fa796c7eee8638b@ec2-54-159-138-67.compute-1.amazonaws.com:5432/dbtt213jnps585');
 
 return [
 
@@ -66,31 +66,31 @@ return [
         ],
 
         'pgsql' => [
-            // 'driver' => 'pgsql',
-            // // 'url' => $Database_url,
-            // 'host' => $Database_url["host"],
-            // 'port' => $Database_url["port"],
-            // 'database' => ltrim($Database_url["path"], "/"),
-            // 'username' => $Database_url["user"],
-            // 'password' => $Database_url["pass"],
-            // 'charset' => 'utf8',
-            // 'prefix' => '',
-            // 'prefix_indexes' => true,
-            // 'schema' => 'public',            
-            // 'sslmode' => 'require'
-
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'url' => $Database_url,
+            'host' => $Database_url["host"],
+            'port' => $Database_url["port"],
+            'database' => ltrim($Database_url["path"], "/"),
+            'username' => $Database_url["user"],
+            'password' => $Database_url["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require'
+
+            // 'driver' => 'pgsql',
+            // 'url' => env('DATABASE_URL'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '5432'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
+            // 'charset' => 'utf8',
+            // 'prefix' => '',
+            // 'prefix_indexes' => true,
+            // 'schema' => 'public',
+            // 'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
